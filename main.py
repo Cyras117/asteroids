1520173a78f2e37884b3fe7cc77f8d1be53339d5
Cyras117 <alexsandro.oliveira117@gmail.com>
2025-07-10 21:15:30 -0400

    limiting frame rate

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ def main():
     Sets up the display surface, processes user events (such as quitting the game),
     clears the screen each frame, and updates the display.
     """
+    pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            
         screen.fill("black")
         pg.display.flip()
-
-
-        
-
+        dt = clock.tick(60)/1000  # Frame rate is set to 60 FPS
 
 
 if __name__ == "__main__":
-    pg.init()
+    
+
     main()
