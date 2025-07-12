99e2b87d9c17a5794e506aca283f3690b7fe5272
Cyras117 <alexsandro.oliveira117@gmail.com>
2025-07-12 00:53:23 -0400

    adding shoot

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS) 
         self.rotation = 0
+        self.bullet_limiter = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,6 +26,8 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if self.bullet_limiter >= 0:
+            self.bullet_limiter -= dt
 
         if keys[pygame.K_w]:
             self.move(dt)
@@ -45,6 +48,8 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(),2)
     
     def shoot(self):
-        s = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        s.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.bullet_limiter <= 0:    
+            s = Shot(self.position.x, self.position.y)
+            s.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.bullet_limiter = PLAYER_SHOOT_COOLDOWN
 
